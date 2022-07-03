7b5dc1c1f3faac237d3cee8d5f916aa60a65159a
Kaan <kaan191@gmail.com>
2022-07-03 22:47:54 +0100

    infinite arena bounds written

--- dogfight.py
+++ dogfight.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import curses
 import os
 from curses import textpad
@@ -27,9 +26,25 @@ PLAYER_ONE_YOKE = {
     curses.KEY_UP: 'down'
 }
 PI = np.pi
+MARGIN = 5
 
 # set env variable so that xterm can show ACS_* curses characters
 os.environ['NCURSES_NO_UTF8_ACS'] = '1'
+
+
+def plus_minus(a, b) -> int:
+    '''Returns +1 if a > b, or -1 if a < b'''
+
+    return ((float(a) > float(b)) - (float(a) < float(b)))
+
+
+def resolve_direction(angle: Radian) -> Vector:
+    '''Converts a Radian value to a Vector with y- and x-values'''
+
+    return np.array([
+        np.round(np.cos(angle), 5),
+        np.round(np.sin(angle), 5)
+    ])
 
 
 @dataclass
@@ -37,11 +52,17 @@ class Arena:
     stdscr: Window
     margin: int
 
+    height: Coordinates = field(init=False)
+    width: Coordinates = field(init=False)
+
     upper_left: Coordinates = None
     bottom_right: Coordinates = None
 
     def __post_init__(self):
         h, w = self.stdscr.getmaxyx()
+
+        self.height = h - (self.margin * 2)
+        self.width = w - (self.margin * 2)
 
         self.upper_left = np.array([self.margin, self.margin])
         self.bottom_right = np.array([h, w]) - self.margin
@@ -63,12 +84,61 @@ class Projectile:
 
     Planes, Machine Gun and Cannon objects inherit from Projectile
     '''
+    arena: Arena
+
     coordinates: Coordinates
     speed: Vector
     angle_of_attack: Radian
     turning_circle: Radian
 
     body: str
+
+    infinite: bool = True
+
+    def change_pitch(self, up: bool) -> None:
+        ''''''
+        if up:
+            self.angle_of_attack += self.turning_circle
+        elif not up:
+            self.angle_of_attack -= self.turning_circle
+
+    def move(self) -> None:
+        '''Updates the coordinates of the Projectile object'''
+
+        self.coordinates += (
+            resolve_direction(self.angle_of_attack) *
+            self.speed
+        )
+        self.hit_boundary()
+
+    def hit_boundary(self) -> None:
+        '''Controls consequences of hitting Arena boundary'''
+
+        hit_y_boundary = False
+        hit_x_boundary = False
+
+        y, x = np.rint(self.coordinates)
+        if y == self.arena.upper_left[0] or y == self.arena.bottom_right[0]:
+            hit_y_boundary = True
+        if x == self.arena.upper_left[1] or x == self.arena.bottom_right[1]:
+            hit_x_boundary = True
+
+        if not hit_y_boundary and not hit_x_boundary:
+            return
+
+        if self.infinite:
+            if hit_y_boundary:
+                self.coordinates[0] = (
+                    y +
+                    (plus_minus(self.arena.height, y) * self.arena.height) -
+                    plus_minus(self.arena.height, y)
+                )
+            elif hit_x_boundary:
+                self.coordinates[1] = (
+                    x +
+                    (plus_minus(self.arena.width, x) * self.arena.width) -
+                    plus_minus(self.arena.width, x)
+                )
 
 
 @dataclass
@@ -85,85 +155,71 @@ class Plane(Projectile):
     '''
 
     body: str = '+'
-    nose: str = '-'
+    nose: str = field(init=False)
     nose_coords: Coordinates = field(init=False)
+
+    infinite: bool = True
 
     def __post_init__(self):
         self.nose_coords = (
             np.rint(self.coordinates) +
             np.rint(resolve_direction(self.angle_of_attack))
         )
+        self.draw_nose()
 
+    def move(self) -> None:
 
-def resolve_direction(angle: float) -> Vector:
+        # call base class move method
+        super().move()
 
-    return np.array([
-        np.round(np.cos(angle), 5),
-        np.round(np.sin(angle), 5)
-    ])
+        # extend base class to also adjust coordinates of "nose"
+        self.nose_coords = (
+            np.rint(self.coordinates) +
+            np.rint(resolve_direction(self.angle_of_attack))
+        )
+        self.draw_nose()
 
+    def draw_nose(self) -> None:
+        r'''
+            \    |    /
+        -+   x   +   x    +-  x   +   x
+                               \  |  /
 
-def change_pitch(plane: Plane, up: bool) -> None:
-    ''''''
-    if up:
-        plane.angle_of_attack += plane.turning_circle
-    elif not up:
-        plane.angle_of_attack -= plane.turning_circle
+        •oOoOo•
+        '''
+        y, x = np.rint(resolve_direction(self.angle_of_attack))
 
-
-def move_plane(plane: Plane) -> None:
-    '''Updates the coordinates of the plane'''
-    plane.coordinates += (
-        resolve_direction(plane.angle_of_attack) *
-        plane.speed
-    )
-    plane.nose_coords = (
-        np.rint(plane.coordinates) +
-        np.rint(resolve_direction(plane.angle_of_attack))
-    )
-
-
-def draw_plane_nose(plane: Plane) -> None:
-    '''
-        \    |    /                     # noqa
-    -+   x   +   x    +-  x   +   x
-                           \  |  /      # noqa
-
-    •oOoOo•
-    '''
-    prev = plane.nose
-    y, x = np.rint(resolve_direction(plane.angle_of_attack))
-    if y == 0:
-        if x == 0:
-            plane.nose = prev
-        else:
-            plane.nose = '-'
-    elif y == 1:
-        if x == 0:
-            plane.nose = '|'
-        elif x == -1:
-            plane.nose = '/'
-        elif x == 1:
-            plane.nose = '\\'
-    elif y == -1:
-        if x == 0:
-            plane.nose = '|'
-        elif x == -1:
-            plane.nose = '\\'
-        elif x == 1:
-            plane.nose = '/'
+        if y == 0:
+            self.nose = '-'
+        elif y == 1:
+            if x == 0:
+                self.nose = '|'
+            elif x == -1:
+                self.nose = '/'
+            elif x == 1:
+                self.nose = '\\'
+        elif y == -1:
+            if x == 0:
+                self.nose = '|'
+            elif x == -1:
+                self.nose = '\\'
+            elif x == 1:
+                self.nose = '/'
 
 
 def main(stdscr: Window):
     # initial settings
-    curses.curs_set(0)
-    stdscr.nodelay(1)
-    stdscr.timeout(60)
+    curses.curs_set(0)  # stops blinking cursor
+    stdscr.nodelay(1)   #
+    stdscr.timeout(60)  # controls refresh rate
 
-    arena = Arena(stdscr, margin=5)
+    # create arena
+    arena = Arena(stdscr, margin=MARGIN)
     arena.draw()
 
+    # create plane
     plane_one = Plane(
+        arena=arena,
         coordinates=np.array(
             [arena.bottom_right[0]/2, arena.bottom_right[1]/4]
         ),
@@ -187,20 +243,19 @@ def main(stdscr: Window):
         # move plane
         for plane in planes:
 
+            # clear previous frame
             stdscr.addch(*np.rint(plane.nose_coords).astype(int), ' ')
             stdscr.addch(*np.rint(plane.coordinates).astype(int), ' ')
 
+            # adjust plane position
             if key in PLAYER_ONE_YOKE.keys():
-                change_pitch(plane, PLAYER_ONE_YOKE[key] == 'up')
-            move_plane(plane)
-
-            draw_plane_nose(plane)
+                plane.change_pitch(PLAYER_ONE_YOKE[key] == 'up')
+            plane.move()
             stdscr.addch(*np.rint(plane.nose_coords).astype(int), plane.nose)
             stdscr.addch(*np.rint(plane.coordinates).astype(int), plane.body)
 
             stdscr.addstr(1, 0, f'angle of attack: {plane.angle_of_attack}')
             stdscr.addstr(2, 0, f'coordinates: {plane.coordinates}')
-            stdscr.addstr(3, 0, f'direction resolved: {resolve_direction(plane.angle_of_attack)}')
 
     stdscr.refresh()
 
